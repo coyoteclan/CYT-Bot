8914e0c6eba23b8402140a1707c110f24d2659af
Mechamorph <118144070+coyoteclan@users.noreply.github.com>
2024-06-23 06:00:50 +0500

    Delete .vscode directory

--- .vscode/do.sh
+++
@@ -1,3 +0,0 @@
-sudo apt update && sudo apt upgrade
-sudo apt install python3 python3-pip python3-venv
-sudo apt install -y git zip unzip openjdk-17-jdk python3-pip autoconf libtool pkg-config zlib1g-dev libncurses5-dev libncursesw5-dev libtinfo5 cmake libffi-dev libssl-dev
